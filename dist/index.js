822cc1c2b7704ff3c3493c221db18e0ad8cc7d59
Pace Kaneshige <pwtkaneshige@gmail.com>
2021-04-09 14:50:28 -0400

    Update Chrome path in Ubuntu

--- dist/index.js
+++ dist/index.js
@@ -28308,7 +28308,7 @@ async function whichChrome() {
   // "chrome.exe" on Windows
   // "Google Chrome" on macOSs
   const executables = {
-    Linux: '/usr/bin/chromium-browser',
+    Linux: '/usr/bin/google-chrome',
     Windows: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
     macOS: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   };
